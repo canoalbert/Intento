0ba53415ae34f2915fbf6289082778c5498f283d
ALBERTO <canoalberto7@gmail.com>
2023-10-20 09:46:49 +0200

    Primer commit

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Usuario} from "../../common/usuarios";
+import {DataService} from "../../services/data.service";
 
 @Component({
   selector: 'app-list',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.page.scss'],
 })
 export class ListPage implements OnInit {
-
-  constructor() { }
+  usuarios: Usuario[] = [];
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.cargarUsuarios()
   }
 
+  private cargarUsuarios() {
+    this.dataService.getUsuarios().subscribe(
+
+    )
+  }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,6 +2,7 @@ import {Component, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Componente} from "../common/interfaces";
+import {Usuario} from "../common/usuarios";
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,8 @@ export class DataService {
   constructor(private http: HttpClient) { }
   getComponentes(): Observable<Componente[]>{
     return this.http.get<Componente[]>("/assets/data/menu.json")
+  }
+  getUsuarios(): Observable<Usuario[]>{
+    return this.http.get<Usuario[]>("/")
   }
 }
